c70abe3f86c46923e69812faf1ad7ada3f7cdf69
Karina <meriucakarina@gmail.com>
2022-09-06 11:49:56 +0300

    sixth task

---
+++ js/task-06.js
@@ -0,0 +1,26 @@
+const inputColor = document.querySelector('#validation-input');
+const inputLenght = inputColor.getAttribute("data-length");
+
+console.log(inputColor);
+console.log(inputLenght);
+
+inputColor.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event) {
+    const lengthOfInput = Number(inputLenght);
+    const userInput = event.currentTarget.value.length;
+
+    if (lengthOfInput === userInput || lengthOfInput > userInput ) {
+        inputColor.classList.add('valid');
+        inputColor.classList.remove('invalid');
+    }
+    if (lengthOfInput < userInput) {
+        inputColor.classList.add('invalid');
+        inputColor.classList.remove('valid'); 
+    }
+}
+
+
+
+
+
