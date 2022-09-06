3c4d8ef4d3bcc439be51351efc50bb9f15cc12ce
Karina <meriucakarina@gmail.com>
2022-09-06 21:30:33 +0300

    eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const elementOfForm = event.currentTarget.elements;
+
+    const email = elementOfForm.email.value;
+    const password = elementOfForm.password.value;
+    
+    const formData = {
+        email, password,
+    };  
+
+    if (email === '' || password === '') {
+        alert("Fields must be filled")
+    } else (
+        console.log(formData), form.reset()
+    );
+}
