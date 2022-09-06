dccfd7018863500cdbb1ad0bf51702894f5d7592
Karina <meriucakarina@gmail.com>
2022-09-06 22:06:32 +0300

    ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  button: document.querySelector('.change-color'),
+  colorSpan: document.querySelector('.color'),
+}
+
+refs.button.addEventListener('click', onColorChange);
+
+function onColorChange(event) {
+  refs.colorSpan.textContent = getRandomHexColor();
+  document.body.style.background = refs.colorSpan.textContent;
+}
